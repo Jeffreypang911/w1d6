56d150a8782d8ae394a1b0b1676d3d2a164ddd67
jeffreypang911 <jeffreypang911@gmail.com>
2018-09-03 05:36:52 +0000

    http example updated

--- https_example.js
+++ https_example.js
@@ -1,3 +1,18 @@
 var http = require('https');
 
 console.log('I did it!');
+
+var options = {
+    options: 'www.example.org',
+    path: '/'
+  };
+
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
